e5136fd86b68d3df927782d925a226abec32eebe
billnhatnguyen <billnhatnguyen@gmail.com>
2025-07-22 13:19:00 -0400

    Bookmaker manager class
    This class allows us to bookmark quotes and later will allow us to save them within our memory.

---
+++ EmBook/Bookmarker.swift
@@ -0,0 +1,45 @@
+//
+//  Bookmarker.swift
+//  EmBook
+//
+//  Created by Bill Nguyen on 2025-07-22.
+//
+
+import Foundation
+import SwiftUI
+
+class Bookmarker: ObservableObject {
+    @Published var bookmarkedQuotes: [BibleQuote] = []{
+        didSet {
+            saveBookmarks()
+        }
+    }
+
+private func saveBookmarks() {
+        if let encoded = try? JSONEncoder().encode(bookmarkedQuotes) {
+            UserDefaults.standard.set(encoded, forKey: "bookmarkedQuotes")
+        }
+    }
+    
+private func loadBookmarks() {
+        if let savedData = UserDefaults.standard.data(forKey: "bookmarkedQuotes") {
+            if let decoded = try? JSONDecoder().decode([BibleQuote].self, from: savedData) {
+                bookmarkedQuotes = decoded
+            }
+        }
+    }
+    
+func toggleBookmark (quote: BibleQuote) {
+        if let index = bookmarkedQuotes.firstIndex(of: quote) {
+        bookmarkedQuotes.remove(at: index)
+    } else {
+        bookmarkedQuotes.append(quote)
+    }
+}
+    
+func isBookmarked(quote: BibleQuote) -> Bool {
+        bookmarkedQuotes.contains(quote)
+    }
+
+    
+}
