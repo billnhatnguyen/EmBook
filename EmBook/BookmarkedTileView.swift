679282c2973403204c6a659f3186dd04ff23c2c1
billnhatnguyen <billnhatnguyen@gmail.com>
2025-07-24 15:07:26 -0400

    Update BookmarkedTileView.swift
    Further separated the quote tiles to make things more organized

--- EmBook/BookmarkedTileView.swift
+++ EmBook/BookmarkedTileView.swift
@@ -15,29 +15,35 @@ public struct BookmarkedTileView: View {
     public var body: some View {
         ScrollView {
             LazyVGrid(columns: columns, spacing: 16) {
-                ForEach(bookmarker.bookmarkedQuotes, id: \.reference) { quote in NavigationLink(destination:insideBookmarked(quote:quote)){
-                    VStack(spacing: 8) {
-                        Text("“\(quote.text)”")
-                            .font(.body)
-                            .lineLimit(4)
-                            .multilineTextAlignment(.leading)
-                            .foregroundColor(.primary)
-                        
-                        Text(quote.formattedReference)
-                            .font(.caption)
-                            .foregroundColor(.secondary)
-                            .frame(maxWidth: .infinity, alignment: .leading)
-                        
-                    }
-                    .padding()
-                    .frame(maxWidth: .infinity, minHeight: 120)
-                    .background(Color.white)
-                    .cornerRadius(12)
-                }
+                ForEach(bookmarker.bookmarkedQuotes, id: \.reference) { quote in quoteTile(for: quote)
                 }
             }
             .padding()
         }
         .navigationTitle("Bookmarked Quotes")
     }
+    
+    @ViewBuilder
+    private func quoteTile(for quote: BibleQuote) -> some View{
+        NavigationLink(destination:InsideBookmarked(bookmarker: bookmarker, quote:quote)){
+            VStack(spacing: 8) {
+                Text("“\(quote.text)”")
+                    .font(.body)
+                    .lineLimit(4)
+                    .multilineTextAlignment(.leading)
+                    .foregroundColor(.primary)
+                
+                Text(quote.formattedReference)
+                    .font(.caption)
+                    .foregroundColor(.secondary)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                
+            }
+            .padding()
+            .frame(maxWidth: .infinity, minHeight: 120)
+            .background(Color.white)
+            .cornerRadius(12)
+        }
+    }
+    
 }
